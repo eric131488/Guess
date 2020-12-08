80df3d4a7fd6ad5897e4d1ce3a63441234ace7d3
Eric <eric@bonraybio.com>
2020-12-08 16:04:34 +0800

    function design, toast, dialog

--- app/src/main/java/com/tom/guess/MainActivity.kt
+++ app/src/main/java/com/tom/guess/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.tom.guess
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "secret: ${secretNumber.secret}")
+    }
 
+    fun check(view: View) {
+        val n = number.text.toString().toInt()
+        Log.d(TAG, "number: $n")
+        val diff = secretNumber.validate(n)
+        var message = "Yes, you got it"
+
+
+        if (diff < 0) {
+            message = "Bigger"
+        } else if (diff > 0) {
+            message = "Smaller"
+        }
+
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
